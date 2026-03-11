7dd944c89719a67f17ef81b70201097e9ef1b69b
ParaicGavin-STUDENT <g00465477@atu.ie>
2026-03-11 17:16:02 +0000

    commit

--- src/main/java/ie/atu/week7solution/exception/GlobalExceptionHandler.java
+++ src/main/java/ie/atu/week7solution/exception/GlobalExceptionHandler.java
@@ -1,4 +1,31 @@
 package ie.atu.week7solution.exception;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+
 public class GlobalExceptionHandler {
-}
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleException(MethodArgumentNotValidException) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors())
+            errors.put(fieldError.getField(), fieldError.getdefaultMessage());
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(ReservationConflictException.class)
+    public ResponseEntity<String>handleConflict(ReservationConflictException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
--- src/main/java/ie/atu/week7solution/service/ReservationService.java
+++ src/main/java/ie/atu/week7solution/service/ReservationService.java
@@ -4,8 +4,24 @@ import ie.atu.week7solution.model.Reservation;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ReservationService {
+
+    private List<Reservation> reservations = new ArrayList<>();
+    private long nextId = 1;
+
+    public ReservationService(ReservationService reservationService) {
+
+        reservation.setReservationId(nextId++);
+
+        for (Reservation existing : reservations){
+            if (existing.getReservationId() == nextId){}
+        }
+    }
+
     public Reservation addReservation(@Valid Reservation reservation) {
 
         return new Reservation();
