2a9b8ec4b42293218543dd1481ed362e5263efc6
ParaicGavin-STUDENT <g00465477@atu.ie>
2026-03-11 14:22:04 +0000

    Commit 1

--- src/main/java/ie/atu/week7solution/model/Reservation.java
+++ src/main/java/ie/atu/week7solution/model/Reservation.java
@@ -1,4 +1,37 @@
 package ie.atu.week7solution.model;
 
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Reservation {
+
+    private Long reservationId;
+
+    @NotBlank(message = "equipmentTag is required")
+    private String equipmentTag;
+
+    @NotBlank(message = "studentEmail is required")
+    @Email(message = "studentEmailmust be a valid email address")
+    private String studentEmail;
+
+    @NotNull(message = "reservationDate is required")
+    private LocalDate reservationDate;
+
+    @Min(value = 0, message = "startHour must be between 0 and 23")
+    @Max(value = 24, message = "startHour must be between 0 and 23")
+    private int startHour;
+
+    @Min(value = 1, message = "durationHours must be between 1 and 24")
+    @Max(value = 24, message = "durationHours must be between 1 and 24")
+    private int durationHours;
 }
