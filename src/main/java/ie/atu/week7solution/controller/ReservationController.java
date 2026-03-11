14d4ca3d6e0c225ded7feec290c44274421e01fc
ParaicGavin-STUDENT <g00465477@atu.ie>
2026-03-11 15:02:39 +0000

    Commit 1

--- src/main/java/ie/atu/week7solution/controller/ReservationController.java
+++ src/main/java/ie/atu/week7solution/controller/ReservationController.java
@@ -1,4 +1,26 @@
 package ie.atu.week7solution.controller;
 
+import ie.atu.week7solution.model.Reservation;
+import ie.atu.week7solution.service.ReservationService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reservations")
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    // Create
+    @PostMapping
+    public ResponseEntity<Reservation> create(@Valid @RequestBody Reservation reservation) {
+        Reservation saved = reservationService.addReservation(reservation);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
 }
--- src/main/java/ie/atu/week7solution/service/ReservationService.java
+++ src/main/java/ie/atu/week7solution/service/ReservationService.java
@@ -1,4 +1,13 @@
 package ie.atu.week7solution.service;
 
+import ie.atu.week7solution.model.Reservation;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReservationService {
+    public Reservation addReservation(@Valid Reservation reservation) {
+
+        return new Reservation();
+    }
 }
